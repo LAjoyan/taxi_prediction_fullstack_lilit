3416e9631129cf30477166a89c255526122ce8b5
LAjoyan <ajoyan.lilit@gmail.com>
2026-03-15 22:26:04 +0100

    fix: adjust path depth for nested pages to resolve ModuleNotFoundError

--- src/taxipred/frontend/pages/Route_Map.py
+++ src/taxipred/frontend/pages/Route_Map.py
@@ -8,7 +8,8 @@ from streamlit_folium import st_folium
 import joblib
 
 CURRENT_DIR = Path(__file__).resolve().parent
-BASE_DIR = CURRENT_DIR.parent.parent.parent
+BASE_DIR = CURRENT_DIR.parent.parent.parent.parent
+
 if str(BASE_DIR) not in sys.path:
     sys.path.insert(0, str(BASE_DIR))
 
@@ -26,32 +27,35 @@ def load_model():
         return None
     return joblib.load(MODEL_PATH)
 
+
 model = load_model()
 
 
-st.set_page_config(page_title='Address Predictor', page_icon='📍')
+st.set_page_config(page_title="Address Predictor", page_icon="📍")
 # CSS to remove extra whitespace and fix the title position
-st.markdown("""
+st.markdown(
+    """
     <style>
         .block-container { padding-top: 2rem !important; }
         h1 { margin-top: 2rem !important; margin-bottom: 1.5rem !important; }
     </style>
-""", unsafe_allow_html=True)
+""",
+    unsafe_allow_html=True,
+)
 
-st.title('📍 Address-to-Address Prediction')
+st.title("📍 Address-to-Address Prediction")
 
 with st.sidebar:
-    st.header('Conditions')
+    st.header("Conditions")
     time_of_day = st.selectbox(
-        'Time of Day', ['Morning', 'Afternoon', 'Evening', 'Night']
+        "Time of Day", ["Morning", "Afternoon", "Evening", "Night"]
     )
-    day_of_week = st.selectbox('Day of Week', ['Weekday', 'Weekend'])
-    traffic = st.selectbox('Traffic', ['Low', 'Medium', 'High'])
-    weather = st.selectbox('Weather', ['Clear', 'Rain', 'Snow'])
-
+    day_of_week = st.selectbox("Day of Week", ["Weekday", "Weekend"])
+    traffic = st.selectbox("Traffic", ["Low", "Medium", "High"])
+    weather = st.selectbox("Weather", ["Clear", "Rain", "Snow"])
 
     st.markdown(
-            """<br>
+        """<br>
             <br>
     <p style='text-align: center;  font-size:20px;'>
     Developed by MLOps student <a href='https://www.linkedin.com/in/lilit-ajoyan-1565b4183/' target='_blank' style='color: #60a5fa; text-decoration: underline;'>
@@ -65,81 +69,85 @@ with st.sidebar:
             
 
     """,
-            unsafe_allow_html=True,
-        )
+        unsafe_allow_html=True,
+    )
 
 
 col_addr1, col_addr2, col_btn = st.columns([2, 2, 1.2])
 
 with col_addr1:
-    from_address = st.text_input("From Address", placeholder="Enter starting point...", label_visibility="collapsed")
+    from_address = st.text_input(
+        "From Address",
+        placeholder="Enter starting point...",
+        label_visibility="collapsed",
+    )
 
 with col_addr2:
-    to_address = st.text_input("To Address", placeholder="Enter destination...", label_visibility="collapsed")
+    to_address = st.text_input(
+        "To Address", placeholder="Enter destination...", label_visibility="collapsed"
+    )
 
 with col_btn:
-    predict_clicked = st.button('Predict Fare', use_container_width=True)
+    predict_clicked = st.button("Predict Fare", use_container_width=True)
 
 if predict_clicked:
     if not from_address or not to_address:
-        st.warning('Please enter both addresses.')
+        st.warning("Please enter both addresses.")
 
     else:
         try:
-            with st.spinner('Calculating ...'):
-                
+            with st.spinner("Calculating ..."):
                 route_data = get_route_data(from_address, to_address)
-                
-                if route_data['distance_km'] > 100:
-                    st.error('🚨 Distance too far! This model is only for city trips under 100km.')
+
+                if route_data["distance_km"] > 100:
+                    st.error(
+                        "🚨 Distance too far! This model is only for city trips under 100km."
+                    )
                 else:
-                    st.session_state['map_route'] = route_data
-                
+                    st.session_state["map_route"] = route_data
 
                     payload = {
-                        'Trip_Distance_km': float(route_data['distance_km']),
-                        'Trip_Duration_Minutes': float(route_data['duration_min']),
-                        'Time_of_Day': time_of_day,
-                        'Day_of_Week': day_of_week,
-                        'Traffic_Conditions': traffic,
-                        'Weather': weather,
+                        "Trip_Distance_km": float(route_data["distance_km"]),
+                        "Trip_Duration_Minutes": float(route_data["duration_min"]),
+                        "Time_of_Day": time_of_day,
+                        "Day_of_Week": day_of_week,
+                        "Traffic_Conditions": traffic,
+                        "Weather": weather,
                     }
-
 
                     if model is not None:
                         X_in = build_features(payload)
                         pred_log = float(model.predict(X_in)[0])
                         pred_price_usd = float(np.expm1(pred_log))
                         pred_price_sek = pred_price_usd * USD_TO_SEK
-                        
-                        st.session_state['map_prediction'] = {
-                            'estimated_price': round(pred_price_sek, 2)
+
+                        st.session_state["map_prediction"] = {
+                            "estimated_price": round(pred_price_sek, 2)
                         }
         except Exception as e:
-            st.error(f'Error: {e}')
+            st.error(f"Error: {e}")
 
 
-if 'map_route' in st.session_state and 'map_prediction' in st.session_state:
-    route = st.session_state['map_route']
-    res = st.session_state['map_prediction']
-    st.success(f'Route Found: {route['distance_km']:.2f} km')
+if "map_route" in st.session_state and "map_prediction" in st.session_state:
+    route = st.session_state["map_route"]
+    res = st.session_state["map_prediction"]
+    st.success(f"Route Found: {route['distance_km']:.2f} km")
 
     m = folium.Map(
         location=[
-            (route['start_lat'] + route['end_lat']) / 2,
-            (route['start_lon'] + route['end_lon']) / 2,
+            (route["start_lat"] + route["end_lat"]) / 2,
+            (route["start_lon"] + route["end_lon"]) / 2,
         ],
         zoom_start=12,
     )
-    folium.PolyLine(route['polyline_latlon'], weight=5).add_to(m)
+    folium.PolyLine(route["polyline_latlon"], weight=5).add_to(m)
     st_folium(m, width=700, height=400)
 
     st.markdown(
-        f'''
+        f"""
     <div style='background-color:#1e293b; padding:5px; border-radius:10px; text-align:center; border:1px solid #3b82f6;'>
-         <h1 style='color:#60a5fa; margin:0;'>{res['estimated_price']:.2f} SEK</h1>
+         <h1 style='color:#60a5fa; margin:0;'>{res["estimated_price"]:.2f} SEK</h1>
      </div>
-            ''',
+            """,
         unsafe_allow_html=True,
     )
-
